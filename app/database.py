30a49e59e787bc922577cc74773e0fa2956f3cc2
Yusuf Syaifudin <yusuf.syaifudin@gmail.com>
2015-03-19 21:57:55 +0700

    remove mysql-python requirements, it doesn't support python 3

--- app/database.py
+++ app/database.py
@@ -1,9 +1,9 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Define the database - we are working with
-# SQLALCHEMY_DATABASE_URI = 'mysql://root:mydb_pwd@localhost:3306/mydb'
-# SQLALCHEMY_DATABASE_URI = 'mysql://root@localhost:3306/flask?charset=utf8'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:5432/flask'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mydb_pwd@localhost:3306/mydb'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/flask?charset=utf8'
+# SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://username:password@localhost:5432/flask'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'test.db')
 DATABASE_CONNECT_OPTIONS = {}
 SQLALCHEMY_ECHO = True
